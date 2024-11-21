b2458682474a74f92e7adf69cd51cb4fae1924c5
jaemin.kim <jmkim648@gmail.com>
2024-11-21 19:02:40 +0900

    :sparkles: Init Apps
    ## 수정한 내용
    - chat, main, user 앱 생성 및 기초 세팅
    - 슈퍼유저 생성, 회원(기본기능) 생성

--- chatsite/chat/urls.py
+++ chatsite/chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = "main"
+app_name = "chat"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.chat_list, name="chat_list"),
 ]
--- chatsite/user/views.py
+++ chatsite/user/views.py
@@ -1,3 +1,19 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+
+signup = CreateView.as_view(
+    form_class=UserCreationForm,
+    template_name="user/form.html",
+    success_url="/user/login/",
+)
+
+login = LoginView.as_view(
+    template_name="user/form.html",
+)
+
+logout = LogoutView.as_view(next_page="/user/login/")
